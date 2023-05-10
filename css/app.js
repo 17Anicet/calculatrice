b571e2cbe3294c1443828fccfbdec42a9dd7ec20
unknown <chizaanicet@gmail.com>
2023-05-10 16:51:10 +0200

    add to my repository

--- css/app.js
+++ css/app.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+const touches = [...document.querySelector('.boutton')];
+const listkeycode = touches.map( touch=> touch.dataset.key);
+
+addEventListener('keydown', (e) => {
+    const value = e.keyCode.toString();
+})
+
+addEventListener('click', (e) =>{
+    const value = e.target.dataset.key;
+})
