1466d092eb47a9990dd1124a1af751f974323642
unknown <chizaanicet@gmail.com>
2023-05-10 17:05:45 +0200

    add to my repository

--- css/app.js
+++ css/app.js
@@ -4,12 +4,23 @@ const screen = document.querySelector('.screen');
 
 addEventListener('keydown', (e) => {
     const value = e.keyCode.toString();
+    calcul(values)
 })
 
 addEventListener('click', (e) =>{
     const value = e.target.dataset.key;
+    calcul(values)
 })
 
 const calcul = (values) =>{
-    if(listkeycode.includes(values))
+    if(listkeycode.includes(values)){
+        switch (values){
+            case '8':
+                screen.textContent = "";
+                break;
+            case '13' :
+                const calculs = eval(screen.textContent);
+                screen.textContent = calculs;    
+        }
+    }
 }
