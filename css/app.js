f1ed99ad029e3d3183126fe9043bf5ebeaa4ed04
unknown <chizaanicet@gmail.com>
2023-05-10 18:27:17 +0200

    add to my repository

--- css/app.js
+++ css/app.js
@@ -1,6 +1,6 @@
-const touches = [...document.querySelector('.boutton')];
-const listkeycode = touches.map( touch=> touch.dataset.key);
-const screen = document.querySelector('.screen');
+const touches = [...document.querySelectorAll('.boutton')];
+const listkeycode = touches.map( touch => touch.dataset.key);
+const ecran = document.querySelector('.screen');
 
 addEventListener('keydown', (e) => {
     const values = e.keyCode.toString();
@@ -16,14 +16,16 @@ const calcul = (values) =>{
     if(listkeycode.includes(values)){
         switch (values){
             case '8':
-                screen.textContent = "";
+                ecran.textContent = "";
                 break;
             case '13' :
-                const calculs = eval(screen.textContent);
-                screen.textContent = calculs;
+                const calculs = eval(ecran.textContent);
+                ecran.textContent = calculs;
                 break;
             default:
-                const indexKeyCode = listkeycode.indexOf(values);       
+                const indexKeyCode = listkeycode.indexOf(values); 
+                const touch = touches[indexKeyCode];   
+                ecran.textContent += touch.innerHTML;
         }
     }
 }
